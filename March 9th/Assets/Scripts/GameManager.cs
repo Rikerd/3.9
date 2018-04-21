4a16cf5e71d86c5b19968e7b011878fd6f98b484
Richard But <butr@uci.edu>
2018-04-20 17:11:25 -0700

    3.9 Unity Project

---
+++ March 9th/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject settingsMenu;
+    public AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        // Create Default Player Prefs
+        if (!PlayerPrefs.HasKey("masterVol"))
+        {
+            PlayerPrefs.SetFloat("masterVol", 0f);
+        }
+    }
+
+    void Start () {
+        // Load Player Prefs
+        audioMixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVol"));
+
+        settingsMenu.SetActive(false);
+    }
+	
+	void Update () {
+		
+	}
+}
